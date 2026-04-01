83ec6060f09e5426f81a195ecea512304f9efe98
21Luke <lukeskar2002@gmail.com>
2026-04-01 18:58:44 -0500

    Q1a attempt 1

---
+++ sudoku1.py
@@ -0,0 +1,20 @@
+import sys
+import clingo 
+from clingo.application import Application, clingo_main
+
+class SudokuApplication(Application):
+    program_name = "sudoku1"
+    
+    
+    def main(self, control: clingo.Control, files):
+        control.load("sudoku.lp")
+        
+        for file_name in files:
+            control.load(file_name)
+            
+        control.ground([("base",[])])
+        control.solve()
+        
+        
+if __name__ == "__main__":
+    sys.exit(clingo_main(SudokuApplication()))
