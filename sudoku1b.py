d67fe7365afc72686b3828fb6c3355355c168289
21Luke <lukeskar2002@gmail.com>
2026-04-01 20:11:42 -0500

    Q1b attempt1

---
+++ sudoku1b.py
@@ -0,0 +1,23 @@
+#Final answer
+import clingo 
+from clingo.application import Application, clingo_main
+
+class SudokuApplication(Application):
+    program_name = "sudoku1"
+
+    def main(self, control: clingo.Control, files):
+        control.load("sudoku.lp")
+        
+        for file_name in files:
+            control.load(file_name)
+            
+        control.ground([("base",[])])
+        control.solve()
+        
+    def print_model(self, model, printer):
+        atoms = model.symbols(shown = True)
+        atoms = sorted(atoms, key = str)
+        printer(" ".join(str(atom) for atom in atoms))
+        
+if __name__ == "__main__":
+    clingo_main(SudokuApplication())
